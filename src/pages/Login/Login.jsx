6caeade08a54a5bb97d5347270bda82013f8c618
drupad <hpdrupad@gmail.com>
2025-08-25 12:24:31 +0530

    Fix responsive scaling: added viewport meta + rem units

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import "./Login.css"
+// import "./Login.css"
 import logo from '../../assets/Dmovieslogo.png'
 import { login,signup } from '../../firebase'
 import loading from '../../assets/loading.gif'
